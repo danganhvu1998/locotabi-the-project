da4c3546363f0fae8d2731bf2093b9c842677462
Dang Anh Vu <danganhvu1998@gmail.com>
2019-10-20 17:03:10 +0900

    Setting i18n and env variables

--- client/config/prod.env.js
+++ client/config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_I18N_LOCALE: '"en"',
+  VUE_APP_I18N_FALLBACK_LOCALE: '"en"',
+  BACKEND_SERVER_ADDRESS: '""'
 }
