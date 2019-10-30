a80b1cf0e71401367d33d3b70c638d2685223120
Dang Anh Vu <danganhvu1998@gmail.com>
2019-10-30 21:08:01 +0900

    prepare request sender center

---
+++ client/src/store/modules/requestSender.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vue from 'vue'
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies, axios, VueAxios)
+
+const actions = {
+}
+
+export default {
+  actions
+}
