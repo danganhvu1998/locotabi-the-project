526b954d00348810e419e201275bb4cf04e92116
hsgser <nguyentuanhaidang@gmail.com>
2019-11-10 11:32:12 +0900

    Allow user to send anonymously
    - Remove authentication guard for feedback in server\routes\api.php
    - Check if user is authenticated in FeedbackController.php

--- server/app/Http/Controllers/FeedbackController.php
+++ server/app/Http/Controllers/FeedbackController.php
@@ -9,9 +9,18 @@ use Illuminate\Http\Request;
 class FeedbackController extends Controller
 {
     public function feedback(Request $request) {
+        if (Auth::check()) {
+            // The user is logged in...
+            return Feedback::create([
+                'name' => Auth::user()->name,
+                'email' => Auth::user()->email,
+                'message' => $request->message
+            ]);
+        }
+        
         return Feedback::create([
-            'name' => Auth::user()->name,
-            'email' => Auth::user()->email,
+            'name' => 'anonymous',
+            'email' => 'anonymous',
             'message' => $request->message
         ]);
         // return response()->json('Your message is sent successfully.', 200);
--- server/routes/api.php
+++ server/routes/api.php
@@ -21,8 +21,11 @@ Route::post("/login", "AuthController@login")->name("login");
 
 Route::post("/register", "AuthController@register")->name("register");
 
+Route::post("/feedback", "FeedbackController@feedback")->name("feedback");
+
 Route::middleware('auth:api')->post("/logout_current", "AuthController@logoutCurrent")->name("logout_current");
 
 Route::middleware('auth:api')->post("/logout_all", "AuthController@logoutAll")->name("logout_all");
 
-Route::middleware('auth:api')->post("/feedback", "FeedbackController@feedback")->name("feedback");
+// Route::middleware('auth:api')->post("/feedback", "FeedbackController@feedback")->name("feedback");
+
